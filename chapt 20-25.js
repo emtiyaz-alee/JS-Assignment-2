2f43bd17610401260a8320f7b18e9f95d820e830
emtiyaz-alee <engineerimtiazali5@gmail.com>
2023-02-20 04:01:08 -0800

    JavaScript Assignment no 2

---
+++ chapt 20-25.js
@@ -0,0 +1,29 @@
+
+//Q1
+// var firstName=prompt("Enter your First Name")
+// var lastName=prompt("Enter your Last Name")
+// var fullName=firstName+" "+lastName
+// alert("Your Full Name is "+" "+fullName)
+
+
+// Q2
+// var favrtMobile=prompt("Enter your Favaurite Mobile Name")
+// var len=favrtMobile.length()
+// // alert("Your favourite Mobile is: "+" "+favrtMobile +"<br>"+"length is:"+" "+len)
+// document.write(len)
+
+// Q3
+// var word="pakistani"
+// var find=word.slice(7,8)
+// document.write(find)
+
+// Q4
+// var word="peanuts"
+// var result=word.toLocaleUpperCase
+// document.write(result)
+
+// Q5
+// var firstName=prompt("Enter your First Name")
+// var lastName=prompt("Enter your Last Name")
+
+// alert("Your Full Name is "+" "+firstName+" "+lastName)
---
+++ chapt 26-34.js
@@ -0,0 +1,46 @@
+// Q1
+// var num=+prompt("Enter a positive Number")
+// var rnd=Math.round(num)
+// var flr=Math.floor(num)
+// var cil=Math.ceil(num)
+// document.write("Number is:"+" "+num+"<br>")
+// document.write("Round off:"+" "+rnd+"<br>")
+// document.write("Floor is:"+" "+flr+"<br>")
+// document.write("Ceil  is:"+" "+cil+"<br>")
+
+
+// Q2
+// var num=+prompt("ENter a Negative Number")
+// var rnd=Math.round(num)
+// var flr=Math.floor(num)
+// var cil=Math.ceil(num)
+// document.write("Number is:"+" "+num+"<br>")
+// document.write("Round off:"+" "+rnd+"<br>")
+// document.write("Floor is:"+" "+flr+"<br>")
+// document.write("Ceil  is:"+" "+cil+"<br>")
+
+// Q3
+// var num=+prompt("ENter a Postive floating point Number")
+// var rnd=Math.round(num)
+// var flr=Math.floor(num)
+// var cil=Math.ceil(num)
+// document.write("Number is:"+" "+num+"<br>")
+// document.write("Round off:"+" "+rnd+"<br>")
+// document.write("Floor is:"+" "+flr+"<br>")
+// document.write("Ceil  is:"+" "+cil+"<br>")
+
+// Q4
+// var num=+prompt("ENter a Negative floating point Number")
+// var rnd=Math.round(num)
+// var flr=Math.floor(num)
+// var cil=Math.ceil(num)
+// document.write("Number is:"+" "+num+"<br>")
+// document.write("Round off:"+" "+rnd+"<br>")
+// document.write("Floor is:"+" "+flr+"<br>")
+// document.write("Ceil  is:"+" "+cil+"<br>")
+
+
+// Q5
+// var num=+prompt("Enter a Number")
+// var abbs=Math.abs(num)
+// document.write("Absolute value is:"+" "+abbs)
